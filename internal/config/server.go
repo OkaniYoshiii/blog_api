cbb1a0fbc8ac54f16628891f76fb160268e59afb
OkaniYoshiii <dragut31620@gmail.com>
2026-01-30 19:55:46 +0100

    feat: simplification configuration serveur

--- internal/config/server.go
+++ internal/config/server.go
@@ -2,40 +2,22 @@ package config
 
 import (
 	"strconv"
-	"time"
 )
 
 type ServerConfig struct {
 	Host string
 	Port int
-	ReadTimeout time.Duration
-	ReadHeaderTimeout time.Duration
-	WriteTimeout time.Duration
-	IdleTimeout time.Duration
 }
 
+const DefaultServerPort = 8000
+
 func ServerConfigFromEnv(env Env) (ServerConfig, error) {
-	port, err := strconv.Atoi(env["SERVER_PORT"])
-	if err != nil {
-		return ServerConfig{}, err
+	var err error
+	port := DefaultServerPort
+	if envPort := env["SERVER_PORT"]; envPort != "" {
+		port, err = strconv.Atoi(envPort)
 	}
 
-	readTimeout, err := strconv.Atoi(env["SERVER_READ_TIMEOUT"])
-	if err != nil {
-		return ServerConfig{}, err
-	}
-
-	readHeaderTimeout, err := strconv.Atoi(env["SERVER_READ_HEADER_TIMEOUT"])
-	if err != nil {
-		return ServerConfig{}, err
-	}
-
-	writeTimeout, err := strconv.Atoi(env["SERVER_WRITE_TIMEOUT"])
-	if err != nil {
-		return ServerConfig{}, err
-	}
-
-	idleTimeout, err := strconv.Atoi(env["SERVER_IDLE_TIMEOUT"])
 	if err != nil {
 		return ServerConfig{}, err
 	}
@@ -43,9 +25,5 @@ func ServerConfigFromEnv(env Env) (ServerConfig, error) {
 	return ServerConfig{
 		Host: env["SERVER_HOST"],
 		Port: port,
-		ReadTimeout: time.Duration(readTimeout),
-		ReadHeaderTimeout: time.Duration(readHeaderTimeout),
-		WriteTimeout: time.Duration(writeTimeout),
-		IdleTimeout: time.Duration(idleTimeout),
 	}, nil
 }
